71fb3fb5784dd300db5f34f1f3926e566dca2c38
Juan <0262576@up.edu.mx>
2025-11-04 11:40:33 -0600

    Added SideBar and made it responsive

--- src/app/SideBar/SideBar.tsx
+++ src/app/SideBar/SideBar.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import React from "react";
-import { Drawer, Button, ListItemButton, ListItemText } from "@mui/material";
-import {List} from "@mui/material";
-import {ListItem} from "@mui/material";
+import { Box, Drawer, IconButton, List, ListItem, ListItemText, useMediaQuery,Button,ListItemButton} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
+import './SideBar.css'
 
 function SideBar(){
 
@@ -14,4 +14,36 @@ function SideBar(){
     setOpen(newOpen);
   };
 
+  const isDesktop = useMediaQuery("(min-width:900px)");
+
+  const drawerContent=(
+    <List>
+      <ListItemButton><ListItemText primary="Inicio" /></ListItemButton>
+      <ListItemButton><ListItemText primary="Foros" /></ListItemButton>
+      <ListItemButton><ListItemText primary="Mi Perfil" /></ListItemButton>
+    </List>
+  )
+  return(
+    <div className="SideBar">
+      {!isDesktop&& (
+        <IconButton className="hamburger-button" onClick={toggleDrawer(true)}>
+          <MenuIcon></MenuIcon>
+        </IconButton>
+      )}
+
+      {isDesktop ? (
+        <Drawer  open classes={{paper:'custom_drawer'}} ModalProps={{hideBackdrop:true}} variant="permanent" className="DrawerDesktop" sx={{'& .MuiDrawer-paper':{backgroundColor: '#525252ff'}}}>
+          {drawerContent}
+        </Drawer>
+      ): (
+        <Drawer  open={open} onClose={toggleDrawer(false)} classes={{paper:'custom_drawer'}} ModalProps={{keepMounted:true}} variant="temporary" sx={{'& .MuiDrawer-paper':{backgroundColor: '#525252ff'}}}>
+          {drawerContent}
+        </Drawer>
+      )}
+
+    </div>
+  )
+
 }
+
+export default SideBar
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,4 @@
+import SideBar from "./SideBar/SideBar";
 import TopBar from "./TopBar/TopBar";
 
 function  App()
@@ -5,6 +6,7 @@ function  App()
     return(
         <div>
             <TopBar></TopBar>
+            <SideBar></SideBar>
         </div>
     )
 }
