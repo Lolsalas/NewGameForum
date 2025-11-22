759f193bc818e0d0adbd3f9fc9a393a4315c1ddc
Juan <0262576@up.edu.mx>
2025-11-22 17:23:39 -0600

    IT WORKS!!! BASIC BETA DONE! GOTTA GET IT CUTE AND THATS IT

--- videogameforum_frontend/src/app/MainForum/MainForumElements.tsx
+++ videogameforum_frontend/src/app/MainForum/MainForumElements.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material"
 import Link from "next/link"
 
 export interface Threads
@@ -16,16 +17,23 @@ export interface Threads
 function MainForumElements({id,title,author,date,replies,postid}:Threads)
 {
     return(
-    <div className="MainForumElement">
-    <div className="MainForumTitleRow">
-        <Link href={`/MainForum/${id}/${postid}`}>
-        <h3 className="MainForumTitle">{title}</h3>
-        </Link>
+    <div className="Container">        
+    <div className="MainForumCreatePostButton"> 
+            <Link href={`/MainForum/${id}/CreatePost`}>Create Post</Link>
     </div>
-    <div className="MainForumTitleMeta">
-        <span className="MainForumInfo">{author}</span>
-        <span className="MainForumInfo">{date}</span>
-        <span className="MainForumInfo">{replies}</span>
+    <div className="MainForumElement">
+        <div className="MainForumTitleRow">
+            <Link href={`/MainForum/${id}/${postid}`}>
+                <h3 className="MainForumTitle">{title}</h3>
+            </Link>
+        </div>
+        <div className="MainForumTitleMeta">
+            <span className="MainForumInfo">{author}</span>
+            <span className="MainForumInfo">{date}</span>
+            <span className="MainForumInfo">{replies}</span>
+        </div>
+
+
     </div>
     </div>
     )
